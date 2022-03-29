76fecda66520639972a501acd963a0c0588fc8b5
coryla <cory.lance.anderson@gmail.com>
2022-03-29 13:30:33 -0500

    token

--- db/index.js
+++ db/index.js
@@ -363,6 +363,25 @@ async function getPostsByTagName(tagName) {
   }
 }
 
+async function getUserByUsername(username) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `
+      SELECT *
+      FROM users
+      WHERE username=$1;
+    `,
+      [username]
+    );
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
 module.exports = {
   client,
   getAllUsers,
@@ -379,4 +398,5 @@ module.exports = {
   createTags,
   getPostsByTagName,
   getAllTags,
+  getUserByUsername,
 };
